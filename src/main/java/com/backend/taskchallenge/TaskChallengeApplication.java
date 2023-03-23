19b8703970ed2059d3526fa11646d620ac2d34be
ChoukseyAnand <anand.chouksey@simpleshow.com>
2023-03-23 06:24:02 +0100

    TASK: refactored for naming conventions

--- src/main/java/com/backend/taskchallenge/TaskChallengeApplication.java
+++ src/main/java/com/backend/taskchallenge/TaskChallengeApplication.java
@@ -1,4 +1,4 @@
-package org.example.taskChallenge;
+package com.backend.taskchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

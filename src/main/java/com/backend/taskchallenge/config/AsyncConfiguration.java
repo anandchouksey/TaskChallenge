ff09d722c92c941520cb78f4bcd7aa9a3106ad24
ChoukseyAnand <anand.chouksey@simpleshow.com>
2023-03-23 06:33:40 +0100

    TASK: properties file and config added

---
+++ src/main/java/com/backend/taskchallenge/config/AsyncConfiguration.java
@@ -0,0 +1,39 @@
+package com.backend.taskchallenge.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+@ConfigurationProperties
+public class AsyncConfiguration {
+    @Value("${task.executor.corePoolSize}")
+    private int corePoolSize;
+
+    @Value("${task.executor.maxPoolSize}")
+    private int maxPoolSize;
+
+    @Value("${task.executor.queueCapacity}")
+    private int queueCapacity;
+
+    @Value("${task.executor.threadNamePrefix}")
+    private String threadNamePrefix;
+
+    @Bean(name = "asyncExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setThreadNamePrefix(threadNamePrefix);
+        executor.initialize();
+        return executor;
+    }
+
+}

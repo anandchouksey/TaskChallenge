e015868adb00784518ca0f1bf9653972b37e34be
ChoukseyAnand <anand.chouksey@simpleshow.com>
2023-03-23 06:18:42 +0100

    TASK: application class added

---
+++ src/main/java/org/example/taskChallenge/TaskChallengeApplication.java
@@ -0,0 +1,14 @@
+package org.example.taskChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TaskChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskChallengeApplication.class, args);
+    }
+}

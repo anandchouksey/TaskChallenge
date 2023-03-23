a322958defaf37255436b42e4c24652b0d356b6d
ChoukseyAnand <anand.chouksey@simpleshow.com>
2023-03-23 07:57:43 +0100

    TASK: controllers added

---
+++ src/main/java/com/backend/taskchallenge/controllers/TaskController.java
@@ -0,0 +1,91 @@
+package com.backend.taskchallenge.controllers;
+
+import com.backend.taskchallenge.model.Task;
+import com.backend.taskchallenge.service.FileService;
+import com.backend.taskchallenge.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class TaskController {
+
+    private final TaskService taskService;
+    private final FileService fileService;
+
+    @Autowired
+    public TaskController(TaskService taskService,
+                          FileService fileService) {
+        this.taskService = taskService;
+        this.fileService = fileService;
+    }
+
+    @GetMapping("/v1/api/tasks")
+    public List<Task> listTasks() {
+        return taskService.listTasks();
+    }
+
+    @PostMapping("/v1/api/tasks")
+    public Task createTask(@RequestBody @Valid Task Task) {
+        return taskService.createTask(Task);
+    }
+
+    @GetMapping("/v1/api/tasks/{taskId}")
+    public Task getTask(@PathVariable String taskId) {
+        return taskService.getTask(taskId);
+    }
+
+    @PutMapping("/v1/api/tasks/{taskId}")
+    public Task updateTask(@PathVariable String taskId,
+                                            @RequestBody @Valid Task Task) {
+        return taskService.update(taskId, Task);
+    }
+
+    @DeleteMapping("/v1/api/tasks/{taskId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTask(@PathVariable String taskId) {
+        taskService.delete(taskId);
+    }
+
+    @GetMapping("/v1/api/tasks/result/{taskId}")
+    public ResponseEntity<FileSystemResource> getResult(@PathVariable String taskId) {
+        return fileService.getTaskResult(taskId);
+    }
+
+    @PostMapping("/v1/api/tasks/execute/{taskId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void executeTask(@PathVariable String taskId) {
+        taskService.executeTask(taskId);
+    }
+
+    @PostMapping("/v2/api/tasks/execute/{taskId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void triggerExecution(@PathVariable String taskId) {
+        taskService.triggerTaskExecution(taskId);
+    }
+
+    @PostMapping("/v2/api/tasks/cancel/{taskId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void cancelTaskExecution(@PathVariable String taskId) {
+        taskService.cancelTaskExecution(taskId);
+    }
+
+    @GetMapping("/v2/api/tasks/status/{taskId}")
+    public Task getExecutionStatus(@PathVariable String taskId) {
+        return taskService.getExecutionStatus(taskId);
+    }
+
+}
